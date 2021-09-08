828fe56e27325fde057f1dedff5b44c190d31c56
Cyclonus931 <78279071+Cyclonus931@users.noreply.github.com>
2021-09-08 16:42:07 +0300

    HW-lesson-8

---
+++ HW 8.py
@@ -0,0 +1,88 @@
+"""
+1. Определение количества различных подстрок с использованием хеш-функции. Пусть дана строка S длиной N. Например,
+состоящая только из маленьких латинских букв. Требуется найти количество различных подстрок в этой строке. Для решения
+задачи рекомендую воспользоваться алгоритмом sha1 из модуля hashlib или встроенную функцию hash()
+
+"""
+
+S = str(input("Введите строку S: "))
+
+print("Строка \'%s\' имеет длину %d сиволов." % (S, len(S)))
+
+subs_set = set()
+# subs_dict = {}
+for i in range(len(S)):
+    for j in range(len(S) - 1 if i == 0 else len(S), i, -1):
+        subs_set.add(hash(S[i:j]))
+        # print(S[i:j], i, j)
+        # subs_dict[S[i:j]] = hash(S[i:j])
+
+# print(len(list(subs_dict.keys())), list(subs_dict.keys()))
+print("Количество различных подстрок в этой строке:", len(subs_set))
+
+'''
+ 2. Закодируйте любую строку (хотя бы из трех слов) по алгоритму Хаффмана.
+'''
+
+from collections import Counter, deque
+
+
+class MyNode:
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+
+def haffman_tree(s):
+    '''
+    Построение дерева для кодирования по алгоритму Хаффмана
+    '''
+
+    count_s = Counter(s)
+
+    sorted_s = deque(sorted(count_s.items(), key=lambda item: item[1]))
+
+    while len(sorted_s) > 1:
+
+        weight = sorted_s[0][1] + sorted_s[1][1]
+        node = MyNode(left=sorted_s.popleft()[0], right=sorted_s.popleft()[0])
+
+        # Вставка пары "узел, вес" на нужное место в список
+        for i, item in enumerate(sorted_s):
+            if weight > item[1]:
+                continue
+            else:
+                sorted_s.insert(i, (node, weight))
+                break
+        else:
+            sorted_s.append((node, weight))
+
+    return sorted_s[0][0]
+
+
+code_table = dict()
+
+
+def haffman_code(tree, path=''):
+    '''
+    Составление таблицы кодирования по дереву
+    '''
+
+    if not isinstance(tree, MyNode):
+        code_table[tree] = path
+
+    else:
+        haffman_code(tree.left, path=f'{path}0')
+        haffman_code(tree.right, path=f'{path}1')
+
+
+s = input('Введите строку для кодировки: ')
+
+# Формирование таблицы кодирования
+haffman_code(haffman_tree(s))
+
+for i in s:
+    print(code_table[i], end=' ')
+
+print()
